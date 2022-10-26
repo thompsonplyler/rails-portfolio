a511a8deda35732f69d6d226c63c783b1196b96c
Thompson Plyler <nealok.g@gmail.com>
2022-10-26 16:25:45 -0400

    First commit.

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  namespace :api do
+    resources :recipes, only: [:index, :create]
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  end
+  # all other routes will be load our React application
+  # this route definition matches:
+  # - *path: all paths not matched by one of the routes defined above
+  # - constraints:
+  #   - !req.xhr?: it's not a XHR (fetch) request
+  #   - req.format.html?: it's a request for a HTML document
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+end
